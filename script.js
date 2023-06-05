eb41d794eb307059948cf36c2807090788544683
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-06-05 06:35:00 +0530

    new

---
+++ script.js
@@ -0,0 +1,559 @@
+const contactsData = [
+  {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Cloud",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Server",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "VMS",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "SAP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Cloud",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Server",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "VMS",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "SAP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Cloud",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Server",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "VMS",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "SAP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Cloud",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Server",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "VMS",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "SAP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "ERP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Cloud",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "Server",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "VMS",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "SAP",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "VMS",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "CRM",
+      registerDate: "04-27-2022",
+    },
+    {
+      email: "Intern1conqt@gmail.com",
+      firstName: "Intern1",
+      lastName: "Techcurve",
+      phone: "7789645632",
+      category: "SAP",
+      registerDate: "04-27-2022",
+    },
+                      
+];
+;
+
+const contactRowsContainer = document.getElementById("contactRows");
+const previousPage = document.getElementById("previousPage");
+const nextPage = document.getElementById("nextPage");
+
+const recordsPerPage = 10;
+let currentPage = 1;
+
+
+function displayContacts() {
+  const startIndex = (currentPage - 1) * recordsPerPage;
+  const endIndex = startIndex + recordsPerPage;
+  const contactsToDisplay = contactsData.slice(startIndex, endIndex);
+
+  contactRowsContainer.innerHTML = ""; // Clear existing rows
+
+  contactsToDisplay.forEach((contact) => {
+    const row = generateTableRow(contact);
+    contactRowsContainer.appendChild(row);
+  });
+
+  updatePagination();
+}
+
+document.addEventListener("DOMContentLoaded", displayContacts);
+
+function updatePagination() {
+  const pageLinks = document.querySelectorAll(".pagination .page-link");
+  pageLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (Number(link.dataset.page) === currentPage) {
+      link.classList.add("active");
+    }
+  });
+
+  previousPage.classList.toggle("disabled", currentPage === 1);
+  nextPage.classList.toggle(
+    "disabled",
+    currentPage === Math.ceil(contactsData.length / recordsPerPage)
+  );
+}
+
+function goToPreviousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    displayContacts();
+  }
+}
+
+function goToNextPage() {
+  if (currentPage < Math.ceil(contactsData.length / recordsPerPage)) {
+    currentPage++;
+    displayContacts();
+  }
+}
+
+
+function generateTableRow(contact) {
+  const row = document.createElement("tr");
+
+  const emptyCell = document.createElement("td");
+  row.appendChild(emptyCell);
+
+  const emailCell = document.createElement("td");
+  emailCell.textContent = contact.email;
+  row.appendChild(emailCell);
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = contact.firstName;
+  row.appendChild(firstNameCell);
+
+  const lastNameCell = document.createElement("td");
+  lastNameCell.textContent = contact.lastName;
+  row.appendChild(lastNameCell);
+
+  const phoneCell = document.createElement("td");
+  phoneCell.textContent = contact.phone;
+  row.appendChild(phoneCell);
+
+  const categoryCell = document.createElement("td");
+  categoryCell.textContent = contact.category;
+  row.appendChild(categoryCell);
+
+  const registerDateCell = document.createElement("td");
+  registerDateCell.textContent = contact.registerDate;
+  row.appendChild(registerDateCell);
+
+  // Add more table cells for additional columns if needed
+
+  // Add a click event listener to the row
+  row.addEventListener("click", showPopup);
+
+  return row;
+}
+
+
+
+// Function to generate table rows based on the contactsData array
+function generateTableRows() {
+    const noDataMessage = document.getElementById("noDataMessage");
+    const contactRowsContainer = document.getElementById("contactRows");
+  contactRowsContainer.innerHTML = ""; // Clear existing rows
+  if (contactsData.length === 0) {
+      noDataMessage.style.display = "table-row";
+    } else {
+      noDataMessage.style.display = "none";
+    }
+  contactsData.forEach((contact) => {
+    const row = generateTableRow(contact);
+    contactRowsContainer.appendChild(row);
+  });
+}
+
+// sidebar function
+document.addEventListener("DOMContentLoaded", function() {
+    var sidebarCollapse = document.getElementById("sidebarCollapse");
+    var sidebar = document.getElementById("sidebar");
+  
+    sidebarCollapse.addEventListener("click", function() {
+      sidebar.classList.toggle("active");
+    });
+  });
+  
+
+
+
+//   new bootstrap code
+function openImportModal() {
+    $('#importModal').modal('show');
+  }
+
+  function submitImportForm() {
+    // Code to handle form submission
+    // You can access the form data and process it here
+
+    // After processing the form data, close the modal
+  //   $('#importModal').modal('hide');
+
+    // Refresh the table rows with updated data
+    generateTableRows();
+
+    displayContacts();
+  //   closeModal();
+
+  }
+
+// end
+
+  function showInsertForm() {
+    const insertForm = document.querySelector('.insert-form');
+    insertForm.style.display = 'block';
+  }
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    const category = document.getElementById('category').value;
+    const registerDate = document.getElementById('registerDate').value;
+
+    // Add the contact to the contactsData array
+    const newContact = {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      category: category,
+      registerDate: registerDate
+    }; 
+
+    contactsData.push(newContact);
+
+    // Reset the form
+    event.target.reset();
+
+    // Generate the updated table rows
+    generateTableRows();
+
+    displayContacts();
+
+
+
+  //   closeModal();
+
+    console.log(contactsData); // Output the contactsData array (for testing)
+  }
+
+ function submitAndCloseForm(){
+
+  var form = document.getElementById("myForm");
+  if (form.checkValidity()) {
+      document.getElementById("closeButton").click();
+  }
+  else {
+      form.reportValidity();
+    }
+
+  }
+
+
+function showPopup() {
+  const popup = document.querySelector(".popup");
+  popup.style.display = "block";
+}
+function goToPage(pageNumber) {
+  currentPage = pageNumber;
+  displayContacts();
+}
